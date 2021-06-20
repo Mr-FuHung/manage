ee02a2d5300eacd6c53dcc08f0a8bbf87c5fde7c
Baron <1002150110@qq.com>
2021-06-21 00:10:00 +0800

    菜单管理静态实现

--- src/api/index.js
+++ src/api/index.js
@@ -5,60 +5,58 @@
 import $ajax from '@/utils/ajax';
 
 const apis = {
-    loginPage: {
-        //登录
-        login: data => $ajax({
-            url: "/users/login",
-            method: "post",
-            mock: false,
-            data,
-        })
-    },
-    userPage: {
-        //用户列表
-        getUserList: data => $ajax({
-            url: "/users/list",
-            data,
-            mock: false,
-        }),
-        //菜单列表
-        getMenuList: () => $ajax({
-            mock: true,
-            url: "/menu/list",
-        }),
-        //待办事项
-        getNoticeCount: () => $ajax({
-            mock: true,
-            url: "/leave/count",
-        }),
-        //删除
-        userDel: data => $ajax({
-            method: "delete",
-            url: "/users/delete",
-            data
-        }),
-        //角色列表
-        getRoleList: () => $ajax({
-            mock: true,
-            url: "/roles/allList",
-        }),
-        //部门列表
-        getDeptList: () => $ajax({
-            mock: true,
-            url: "/dept/list",
-        }),
-        //新增,修改用户
-        userSubmit: (data) => $ajax({
-            url: "/users/operate",
-            method: 'post',
-            data
-        })
-    }
+    //登录
+    login: data => $ajax({
+        url: "/users/login",
+        method: "post",
+        mock: false,
+        data,
+    }),
+    //用户列表
+    getUserList: data => $ajax({
+        url: "/users/list",
+        data,
+        mock: false,
+    }),
+    //菜单列表
+    getMenuList: () => $ajax({
+        mock: true,
+        url: "/menu/list",
+    }),
+    //待办事项
+    getNoticeCount: () => $ajax({
+        mock: true,
+        url: "/leave/count",
+    }),
+    //删除
+    userDel: data => $ajax({
+        method: "delete",
+        url: "/users/delete",
+        data
+    }),
+    //角色列表
+    getRoleList: () => $ajax({
+        mock: true,
+        url: "/roles/allList",
+    }),
+    //部门列表
+    getDeptList: () => $ajax({
+        mock: true,
+        url: "/dept/list",
+    }),
+    //新增,修改用户
+    userSubmit: (data) => $ajax({
+        url: "/users/operate",
+        method: 'post',
+        data
+    }),
+    //新增,修改菜单
+    menuSubmit: (data) => $ajax({
+        url: "/menu/operate",
+        method: 'post',
+        data,
+        mock:true
+    })
 
 }
-let apiAll = {};
-for (const keys in apis) {
-    apiAll = Object.assign(apiAll, apis[keys])
-}
-
-export default apiAll;
+export default apis;
---
+++ src/views/Menu.vue
@@ -0,0 +1,311 @@
+<template>
+  <div class="user-mangn">
+    <div class="query-form">
+      <el-form inline :model="queryForm" ref="form">
+        <el-form-item label="菜单名称" prop="menuName">
+          <el-input v-model="queryForm.menuName" placeholder="请输入菜单名称" />
+        </el-form-item>
+        <el-form-item label="菜单状态" prop="menuState">
+          <el-select
+            v-model="queryForm.menuState"
+            size="small"
+            placeholder="请选择菜单状态"
+          >
+            <el-option :value="1" label="正常"></el-option>
+            <el-option :value="2" label="停用"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="getTableData">查询</el-button>
+          <el-button @click="handleReset('form')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="base-table">
+      <div class="action">
+        <el-button type="primary" @click="handleAdd(1)"> 创建 </el-button>
+      </div>
+
+      <el-table
+        stripe
+        size="medium"
+        :data="tableData"
+        row-key="_id"
+        :tree-props="{ children: 'children' }"
+      >
+        <el-table-column
+          v-for="item in tableHeaderData"
+          :key="item.prop"
+          :prop="item.prop"
+          :label="item.label"
+          :formatter="item.formatter"
+        />
+        <el-table-column label="操作" width="300">
+          <template #default="scope">
+            <el-button type="primary" @click="handleAdd(2, scope.row)">
+              新增
+            </el-button>
+            <el-button @click="handleEdit(scope.row)"> 编辑 </el-button>
+            <el-button type="danger" @click="handleDel(scope.row)">
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <!-- 新增弹窗开始 -->
+    <el-dialog title="新增用户" v-model="showDialog">
+      <el-form
+        ref="dialogMenuForm"
+        size="medium"
+        :model="addMenuForm"
+        label-width="1.2rem"
+        :rules="rules"
+      >
+        <el-form-item label="父级菜单" prop="parentId">
+          <el-cascader
+            style="width: 4rem"
+            v-model="addMenuForm.parentId"
+            placeholder="请选择父级菜单"
+            :options="tableData"
+            :props="{ checkStrictly: true, label: 'menuName', value: '_id' }"
+            clearable
+          />
+          <span>不选，则创建一级菜单</span>
+        </el-form-item>
+        <el-form-item label="菜单类型" prop="menuType">
+          <el-radio-group v-model="addMenuForm.menuType">
+            <el-radio :label="1">菜单</el-radio>
+            <el-radio :label="2">按钮</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="菜单名称" prop="menuName">
+          <el-input
+            v-model.trim="addMenuForm.menuName"
+            placeholder="请输入菜单名称"
+          />
+        </el-form-item>
+        <el-form-item
+          label="菜单图标"
+          prop="icon"
+          v-show="addMenuForm.menuType == 1"
+        >
+          <el-input v-model="addMenuForm.icon" placeholder="请输入菜单图标" />
+        </el-form-item>
+        <el-form-item
+          label="路由地址"
+          prop="path"
+          v-show="addMenuForm.menuType == 1"
+        >
+          <el-input v-model="addMenuForm.path" placeholder="请输入路由地址" />
+        </el-form-item>
+        <el-form-item
+          label="权限标识"
+          prop="menuCode"
+          v-show="addMenuForm.menuType == 2"
+        >
+          <el-input
+            v-model="addMenuForm.menuCode"
+            placeholder="请输入权限标识"
+          />
+        </el-form-item>
+        <el-form-item
+          label="组件路径"
+          prop="component"
+          v-show="addMenuForm.menuType == 1"
+        >
+          <el-input
+            v-model="addMenuForm.component"
+            placeholder="请输入组件路径"
+          />
+        </el-form-item>
+
+        <el-form-item label="菜单状态" prop="menuState">
+          <el-radio-group v-model="addMenuForm.menuState">
+            <el-radio :label="1">正常</el-radio>
+            <el-radio :label="2">停用</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="handleClose">取 消</el-button>
+          <el-button type="primary" @click="handleSubmit">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
+    <!-- 新增弹窗结束 -->
+  </div>
+</template>
+
+<script>
+import utils from "@/utils/utils.js";
+// reactive//用来创建引用类型
+// ref//用来创建基础类型,需用.size配合使用
+//toRaw 响应式对象 转为普通对象
+//getCurrentInstance  初始化一个实例，拿到vue3上下文的配置，ctx
+export default {
+  name: "Menu",
+  data() {
+    return {
+      //查询条件
+      queryForm: {
+        menuState: 1,
+      },
+      //新增
+      showDialog: false,
+      addMenuForm: {
+        menuState: 1,
+        menuType: 1,
+      },
+      rules: {
+        menuName: [
+          {
+            required: true,
+            message: "请输入菜单名称",
+            trigger: "blur",
+          },
+          {
+            min: 2,
+            max: 8,
+            message: "菜单名称长度在2-8个字符之间",
+            trigger: ["blur", "change"],
+          },
+        ],
+      },
+      //表格
+      tableHeaderData: [
+        {
+          label: "菜单名称",
+          prop: "menuName",
+          width: 300,
+        },
+        {
+          label: "图标",
+          prop: "icon",
+          width: 180,
+        },
+        {
+          label: "菜单类型",
+          prop: "menuType",
+          width: 180,
+          formatter(row, column, value) {
+            return {
+              1: "菜单",
+              2: "按钮",
+            }[value];
+          },
+        },
+        {
+          label: "权限标识",
+          prop: "menuCode",
+          width: 180,
+        },
+        {
+          label: "路由地址",
+          prop: "path",
+          width: 180,
+        },
+        {
+          label: "组件路径",
+          prop: "component",
+          width: 180,
+        },
+        {
+          label: "菜单状态",
+          prop: "menuState",
+          width: 90,
+          formatter(row, column, value) {
+            return {
+              1: "正常",
+              2: "停用",
+            }[value];
+          },
+        },
+        {
+          label: "创建时间",
+          prop: "createTime",
+          width: 180,
+          formatter(row, column, value) {
+            return utils.formateDate(new Date(value));
+          },
+        },
+      ],
+      tableData: [],
+      //新增或修改
+      action: "add",
+    };
+  },
+  mounted() {
+    this.getTableData();
+  },
+  methods: {
+    getTableData() {
+      this.$api.getMenuList(this.queryForm).then((list) => {
+        this.tableData = list.data;
+      });
+    },
+    //重置
+    handleReset(form) {
+      this.$refs[form].resetFields();
+    },
+    handleClose() {
+      this.showDialog = false;
+      this.handleReset("dialogMenuForm");
+    },
+    //新增弹窗
+    handleAdd(type, row) {
+      this.action = "add";
+      this.showDialog = true;
+      if (type === 2) {
+        this.addMenuForm.parentId = [...row.parentId, row._id].filter(
+          (value) => value
+        );
+      }
+    },
+    //提交
+    handleSubmit() {
+      //待优化
+      this.$refs.dialogMenuForm.validate(async (valid) => {
+        if (valid) {
+          let { addMenuForm, action } = this;
+          const params = { ...addMenuForm, action };
+          let { msg } = await this.$api.menuSubmit(params);
+          this.showDialog = false;
+          this.$message.success(msg);
+          this.handleReset("dialogMenuForm");
+          this.getTableData();
+        }
+      });
+    },
+    //编辑
+    handleEdit(row) {
+      this.showDialog = true;
+      this.action = "edit";
+      console.log(row);
+      this.$nextTick(() => {
+        Object.assign(this.addMenuForm, row);
+        this.addMenuForm.parentId = [...row.parentId, row._id].filter(
+          (value) => value
+        );
+      });
+    },
+    //删除
+    handleDel(_id) {
+      let { msg } = await this.$api.menuSubmit({ _id, action: "delete" });
+      this.$message.success(msg);
+      this.getTableData();
+    },
+  },
+};
+</script>
+
+<style lang='scss'>
+.el-form--inline .el-form-item {
+  margin-right: 0.3rem;
+}
+.el-dialog .el-select .el-input {
+  width: 130px;
+}
+</style>
